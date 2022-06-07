fb6dfc7be7c297d0fb11a75c0b314c0e89fa8405
Zachary Drayer <zacharydrayer@gmail.com>
2022-06-06 18:36:53 -0700

    maybe run on macOS 13?

---
+++ weather/AppDelegate.swift
@@ -0,0 +1,186 @@
+import Cocoa
+import CoreLocation
+import WeatherKit
+
+
+@main
+class AppDelegate : NSObject, CLLocationManagerDelegate, NSMenuDelegate, NSApplicationDelegate {
+	@IBOutlet
+	private var window: NSWindow!
+
+	private var statusItem: NSStatusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+	private var manager = CLLocationManager()
+	@NSCopying private var placemark: CLPlacemark? = nil
+	private var currentWeatherData: Weather? = nil
+	private var activeTimer: Timer? = nil
+
+	func applicationWillFinishLaunching(_ notification: Notification) {
+		manager.delegate = self
+		manager.distanceFilter = 1000.0
+		manager.desiredAccuracy = kCLLocationAccuracyKilometer
+		manager.pausesLocationUpdatesAutomatically = false
+
+		statusItem.button?.imagePosition = .imageLeft
+		statusItem.button?.imageHugsTitle = true
+	}
+
+	func applicationDidFinishLaunching(_ notification: Notification) {
+		DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) { [unowned self] in
+			manager.requestAlwaysAuthorization()
+			manager.startUpdatingLocation()
+		}
+	}
+
+	func menuWillOpen(_ menu: NSMenu) {
+		statusItem.button?.image?.isTemplate = true
+	}
+
+	func menuDidClose(_ menu: NSMenu) {
+		statusItem.button?.image?.isTemplate = false
+	}
+
+	func buildMenuWithCurrentWeatherData(weather: Weather?, placemark: CLPlacemark?) {
+		guard let weather else {
+			return
+		}
+
+		statusItem.button?.image = NSImage(systemSymbolName: weather.currentWeather.symbolName, accessibilityDescription: weather.currentWeather.condition.rawValue)
+
+		let measurementFormatter = MeasurementFormatter()
+		statusItem.button?.title = String(format: " %@° and %@", measurementFormatter.string(from:  weather.currentWeather.temperature), weather.currentWeather.condition.rawValue).lowercased()
+
+		let menu = NSMenu()
+		menu.delegate = self
+
+		var addSeparator = true
+		if let placemark {
+			if let areaOfInterest = placemark.areasOfInterest?.first, !areaOfInterest.isEmpty {
+				menu.addItem(withTitle: areaOfInterest, action: nil, keyEquivalent: "")
+			} else if let subLocality = placemark.subLocality, !subLocality.isEmpty {
+				menu.addItem(withTitle: subLocality, action: nil, keyEquivalent: "")
+			} else if let locality = placemark.locality, !locality.isEmpty {
+				menu.addItem(withTitle: locality, action: nil, keyEquivalent: "")
+			} else if let subAdministrativeArea = placemark.subAdministrativeArea, !subAdministrativeArea.isEmpty {
+				menu.addItem(withTitle: subAdministrativeArea, action: nil, keyEquivalent: "")
+			} else if let subAdministrativeArea = placemark.subAdministrativeArea, !subAdministrativeArea.isEmpty {
+				menu.addItem(withTitle: subAdministrativeArea, action: nil, keyEquivalent: "")
+			} else if let postalCode = placemark.postalCode, !postalCode.isEmpty {
+				menu.addItem(withTitle: postalCode, action: nil, keyEquivalent: "")
+			} else {
+				addSeparator = false
+			}
+		}
+
+		if addSeparator {
+			menu.addItem(.separator())
+		}
+
+		let formatter = DateFormatter()
+		formatter.formattingContext = .listItem
+		formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "hh a", options: 0, locale: .autoupdatingCurrent)
+
+		let hhmmFormatter = DateFormatter()
+		hhmmFormatter.formattingContext = .listItem
+		hhmmFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "hh: mm a", options: 0, locale: .autoupdatingCurrent)
+
+		var menuItemsByTimestamp = [Date: NSMenuItem]()
+
+		// add all hourly forecasts
+		for forecast in weather.hourlyForecast {
+			let time = formatter.string(from: forecast.date).lowercased()
+			let title = String(format: "%@: %.0f° and %@", time, measurementFormatter.string(from:  forecast.temperature), forecast.condition.rawValue).lowercased()
+			let item = NSMenuItem(title: title, action: nil, keyEquivalent: "")
+			item.representedObject = forecast
+
+			menuItemsByTimestamp[forecast.date] = item
+		 }
+
+		// add any upcoming sunrises or sunsets
+		for forecast in weather.dailyForecast {
+			if let sunrise = forecast.sun.sunrise {
+				let sunriseTitle = String(format: "Sunrise ↑ %@", hhmmFormatter.string(from: sunrise).lowercased())
+				let sunriseItem = NSMenuItem(title: sunriseTitle, action: nil, keyEquivalent: "")
+				sunriseItem.representedObject = forecast
+
+				menuItemsByTimestamp[sunrise] = sunriseItem
+			}
+
+			if let sunset = forecast.sun.sunset {
+				let sunsetTitle = String(format: "Sunset ↓ %@", hhmmFormatter.string(from: sunset).lowercased())
+				let sunsetItem = NSMenuItem(title: sunsetTitle, action: nil, keyEquivalent: "")
+				sunsetItem.representedObject = forecast
+
+				menuItemsByTimestamp[sunset] = sunsetItem
+			}
+		 }
+
+		let fifteenMinutesAgo = Date(timeIntervalSinceNow: -15 * 60)
+
+		var previousKey: Date? = nil
+		for key in menuItemsByTimestamp.keys.sorted(by: { x, y in x > y }) {
+			if fifteenMinutesAgo > key {
+				continue
+			}
+
+			// always show the first item + show any other item that is an hourly forecast, or on a timestamp between two hourly forecasts
+			if previousKey == nil || key.timeIntervalSince(previousKey!) <= 3600 {
+				previousKey = key
+
+				menu.addItem(menuItemsByTimestamp[key]!)
+			}
+		 }
+
+		menu.addItem(.separator())
+		menu.addItem(withTitle: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+
+		statusItem.menu = menu
+	}
+
+	func fetchWeatherDataForLocation(location: CLLocation?) async {
+		guard let location else { return }
+
+		let weather = try? await WeatherService.shared.weather(for: location)
+		self.currentWeatherData = weather
+		self.buildMenuWithCurrentWeatherData(weather: weather, placemark: placemark)
+	}
+
+	func geocodeLocation(location: CLLocation?) {
+		guard let location else { return }
+
+		CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+			DispatchQueue.main.async { [unowned self] in
+				placemark = placemarks?.first
+
+				buildMenuWithCurrentWeatherData(weather: currentWeatherData, placemark: placemark)
+			}
+		}
+	}
+
+	func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+		manager.startUpdatingLocation()
+	}
+
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		Task { await fetchWeatherDataForLocation(location: manager.location) }
+		geocodeLocation(location: locations.last)
+		scheduleUpdate()
+	}
+
+	@objc func updateMenu() {
+		Task { await fetchWeatherDataForLocation(location: manager.location) }
+		scheduleUpdate()
+	}
+
+	func scheduleUpdate() {
+		activeTimer?.invalidate()
+
+		var plusOneHour = DateComponents()
+		plusOneHour.hour = 1
+
+		let nextPlusAnHour = Calendar.current.date(byAdding: plusOneHour, to: .now)!
+		let nextPlusAnHourComponents = Calendar.current.dateComponents([ .month, .year, .hour ], from: nextPlusAnHour)
+		let next = Calendar.current.date(from: nextPlusAnHourComponents)!
+
+		activeTimer = Timer.scheduledTimer(timeInterval: next.timeIntervalSinceNow, target: self, selector: #selector(updateMenu), userInfo: nil, repeats: false)
+	}
+}
---
+++ weather/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by z on 6/6/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
